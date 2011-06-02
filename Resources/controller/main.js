bbe7092458be8d320b36d245a571c36f3606ce75
Jernej Zorec <jernejz@gmail.com>
2011-06-02 22:01:54 +0200

    Removed settings because we don't really need it. Added event for tab switching.

--- Resources/controller/main.js
+++ Resources/controller/main.js
@@ -1,3 +1,20 @@
 Titanium.include ('model/main.js');
 Titanium.include ('view/main.js');
  
+//
+// TAB EVENTS
+//
+
+// tab group close event
+tabGroup.addEventListener('close', function(e)
+{
+	if (Ti.Platform.osname == "iphone") {
+//On iOS, when we're closing the tab group, this is a result
+//of the tab group example of 'Close/Animate Tab Group' and
+//we want to reopen the tab group so the user can continue with
+//using Kitchen Sink. HOWEVER, on Android, this is also triggered
+//when the app is being closed via back button, where reopening
+//the tab group is not desired. This is purely a quirk of the tests.
+		tabGroup.open();
+	}
+});
